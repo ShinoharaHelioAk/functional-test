a44773826abdfd7f3d9abb716979a8b54042c575
Hélio Shinohara <strokestyle.shinohara@gmail.com>
2021-03-16 17:13:52 -0300

    Inclusão do Tipo de Execução para o Selenium GRID.

--- src/test/java/br/com/helio/tasks/core/Propriedades.java
+++ src/test/java/br/com/helio/tasks/core/Propriedades.java
@@ -1,9 +1,12 @@
 package br.com.helio.tasks.core;
 
 import br.com.helio.tasks.enums.Browsers;
+import br.com.helio.tasks.enums.TipoExecucao;
 
 public class Propriedades {
 	public static boolean FECHAR_BROWSER = true;
 	
-	public static Browsers browser = Browsers.CHROME;
+	public static Browsers BROWSER = Browsers.CHROME;
+	
+	public static TipoExecucao TIPO_EXECUCAO = TipoExecucao.GRID;
 }
